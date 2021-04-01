081ac5de2bf4f44bd2bf8cf7f68100a3823b3893
samuelige <samuelige.iyanu@gmail.com>
2021-04-01 22:47:26 +0100

    A complete responsive landing page

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin:10,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        544: {
+            items:2
+        }
+
+
+    }
+})
